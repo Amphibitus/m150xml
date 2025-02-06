1439d7a38a5bae90a578c6fa41ebc706adc056be
Gerd Dreier <amphibitus@gmx.de>
2025-02-06 11:56:50 +0100

    Add files via upload

--- myp.bat
+++ myp.bat
@@ -1,9 +1,16 @@
 @echo off
-call "C:\OSGeo4W64\bin\o4w_env.bat"
-call "C:\OSGeo4W64\bin\qt5_env.bat"
-call "C:\OSGeo4W64\bin\py3_env.bat"
+c:
+SET OSGEO4W_ROOT=C:\OSGeo4W
+SET QGISNAME=qgis
+call "%OSGEO4W_ROOT%"\bin\o4w_env.bat
 
 @echo on
-call "C:\OSGeo4W64\apps\Python37\Scripts\pyrcc5" -o resources.py resources.qrc
+call "%OSGEO4W_ROOT%\apps\Python312\Scripts\pyrcc5" -o resources.py resources.qrc
 
-pause
+
+del /s /q __pycache__\*.*
+rmdir /s /q __pycache__
+
+del /s /q PyPDF2\PyPDF2\__pycache__\*.*
+rmdir /s /q PyPDF2\PyPDF2\__pycache__
+rem pause
--- resources.py
+++ resources.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created by: The Resource Compiler for PyQt5 (Qt v5.11.2)
+# Created by: The Resource Compiler for PyQt5 (Qt v5.15.13)
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -154,7 +154,7 @@ qt_resource_struct_v2 = b"\
 \x00\x00\x00\x14\x00\x02\x00\x00\x00\x01\x00\x00\x00\x03\
 \x00\x00\x00\x00\x00\x00\x00\x00\
 \x00\x00\x00\x28\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
-\x00\x00\x01\x76\x15\x38\x06\xd0\
+\x00\x00\x01\x81\xf3\x17\xf5\x11\
 "
 
 qt_version = [int(v) for v in QtCore.qVersion().split('.')]
